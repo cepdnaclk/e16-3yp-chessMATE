2baa7da3fcf70b33d459bfdfb15c735cddd25b6d
DamsyDeSilva <damsydesilva@gmail.com>
2020-11-08 03:31:07 +0530

    buidOptionButtons method implemented

--- Software/chessMATE_app/lib/chessGame/buildGame.dart
+++ Software/chessMATE_app/lib/chessGame/buildGame.dart
@@ -2,21 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_chess_board/flutter_chess_board.dart';
 
-class PlayGame extends StatefulWidget{
+class PlayGame extends StatefulWidget {
   @override
   _PlayGameState createState() => _PlayGameState();
 }
 
-class _PlayGameState extends State<PlayGame>{
-
+class _PlayGameState extends State<PlayGame> {
   static ChessBoardController controller;
   static List<String> gameHistory;
 
   // will call this method exactly once for each [State] object it creates.
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);  // fix the orientation up for this game interface
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]); // fix the orientation up for this game interface
     controller = new ChessBoardController();
     gameHistory = [];
   }
@@ -29,18 +30,16 @@ class _PlayGameState extends State<PlayGame>{
           title: const Text('ChessMATE'),
         ),
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              _buildChessBoard(),
-              // -buildOptionButtons(),
-              Expanded(
-                child: FittedBox(
+          child: Column(children: <Widget>[
+            _buildChessBoard(),
+            _buildOptionButtons(),
+            Expanded(
+              child: FittedBox(
                 fit: BoxFit.contain,
-                  // child: _buildGameHistory(),
-                ),
+                // child: _buildGameHistory(),
               ),
-            ]
-          ),
+            ),
+          ]),
         ),
       ),
     );
@@ -56,7 +55,7 @@ class _PlayGameState extends State<PlayGame>{
           gameHistory.add(moveNotation);
           setState(() {});
         },
-        onCheckMate: (winColor) { 
+        onCheckMate: (winColor) {
           // --> todo : pass the winning deatils to results screen
           // Navigator.pushNamed(context, ResultsScreen.id);
         },
@@ -68,4 +67,40 @@ class _PlayGameState extends State<PlayGame>{
     );
   }
 
+  // method to return the widget containing option buttons
+  Widget _buildOptionButtons() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FlatButton(
+                  child: Text("Reset game"),
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  onPressed: () {
+                    // _resetGame();
+                  },
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FlatButton(
+                  child: Text("undo Move"),
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  onPressed: () {
+                    // _undoMove();
+                  },
+                ),
+              ),
+            ),
+          ]),
+    );
+  }
 }
