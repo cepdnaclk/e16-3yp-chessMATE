6e6034af03430ce79d9dc298af8da44194c1b761
Isurika <123csissa@gmail.com>
2020-11-08 17:44:59 +0530

    Modified main.dart to implement options_screen

--- Software/chessMATE_app/lib/main.dart
+++ Software/chessMATE_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:chessMATE_app/screens/gameScreen.dart';
 import 'package:chessMATE_app/screens/game_mode_screen.dart';
 import 'package:chessMATE_app/screens/playerDetails_screen.dart';
+import 'package:chessMATE_app/screens/option_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         // LoginScreen.id: (context) => LoginScreen(),
         // ForgotPassScreen.id: (context) => ForgotPassScreen(),
         GameScreen.id: (context) => GameScreen(),
-        // OptionScreen.id: (context) => OptionScreen(),
+        OptionScreen.id: (context) => OptionScreen(),
         GameModeScreen.id: (context) => GameModeScreen(),
         // ResultsScreen.id: (context) => ResultsScreen(),
         // SignInScreen.id: (context) => SignInScreen(),
