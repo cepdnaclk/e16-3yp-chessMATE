fa1e143e4a7ac58cdbfa66eed1de1a22d74ba0a4
isu92neth <123csissa@gmail.com>
2020-11-13 00:08:56 +0530

    Add background

--- Software/chessMATE_app/lib/screens/game_mode_screen.dart
+++ Software/chessMATE_app/lib/screens/game_mode_screen.dart
@@ -1,18 +1,14 @@
 import 'package:chessMATE_app/game_mode/body_game_mode.dart';
 import 'package:flutter/material.dart';
 
-
-
 class GameModeScreen extends StatelessWidget {
-
   static const String id = 'GameModeScreen';
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
+      backgroundColor: Color.fromRGBO(4, 7, 40, 90),
       body: GameModeBody(),
     );
   }
 }
-
