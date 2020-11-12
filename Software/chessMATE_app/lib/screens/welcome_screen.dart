9967993b68eddb65800ce527d706e0ef0b043033
isu92neth <123csissa@gmail.com>
2020-11-13 00:16:51 +0530

    Add background

--- Software/chessMATE_app/lib/screens/welcome_screen.dart
+++ Software/chessMATE_app/lib/screens/welcome_screen.dart
@@ -1,16 +1,13 @@
 import 'package:chessMATE_app/welcome/body_welcome.dart';
 import 'package:flutter/material.dart';
 
-
-
 class WelcomeScreen extends StatelessWidget {
-
   static const String id = 'WelcomeScreen';
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
+      backgroundColor: Color.fromRGBO(4, 7, 40, 90),
       body: WelcomeBody(),
     );
   }
