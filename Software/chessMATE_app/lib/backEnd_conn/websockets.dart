9f3bff6a91fb3beab6225c8611575f4c60976ac4
DamsyDeSilva <damsydesilva@gmail.com>
2021-01-30 19:57:18 +0530

    created the websocket.dart and define basic parameters

---
+++ Software/chessMATE_app/lib/backEnd_conn/websockets.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/foundation.dart';
+import 'package:web_socket_channel/io.dart';
+
+// substitute your server's IP and port
+const YOUR_SERVER_IP = '192.168.8.101';
+const YOUR_SERVER_PORT = '3000';
+const String _SERVER_ADDRESS = 'ws://$YOUR_SERVER_IP:$YOUR_SERVER_PORT';
+
+
+class WebSocketsNotifications {
+  static final WebSocketsNotifications _sockets = new WebSocketsNotifications._internal();
+
+  factory WebSocketsNotifications(){
+    return _sockets;
+  }
+
+  WebSocketsNotifications._internal();
+
+  // The WebSocket "open" channel
+  IOWebSocketChannel _channel;
+
+  // Is the connection established?
+  bool _isOn = false;
+  
+  // Listeners 
+  // List of methods to be called when a new message comes in.
+  ObserverList<Function> _listeners = new ObserverList<Function>();
+
+}
