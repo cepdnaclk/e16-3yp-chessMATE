9aa94d58fadcf264dc193bc8468a8a4904a0e2c3
DamsyDeSilva <damsydesilva@gmail.com>
2021-01-30 22:21:32 +0530

    implemet _onAction function to display a movement recieved from server

--- Software/chessMATE_app/lib/chessGame/buildGame.dart
+++ Software/chessMATE_app/lib/chessGame/buildGame.dart
@@ -34,8 +34,38 @@ class _PlayGameState extends State<PlayGame> {
       DeviceOrientation.portraitUp
     ]); // fix the orientation up for this game interface
     controller = ChessBoardController();
+    
     // Ask to be notified when a message from the server comes in.
-    // game.addListener(_onAction);
+    game.addListener(_onAction);
+  }
+
+  @override
+  void dispose(){
+    game.removeListener(_onAction);
+    super.dispose();
+  }
+
+  // ---------------------------------------------------------
+  // The opponent took an action
+  // Handler of these actions
+  // ---------------------------------------------------------
+  _onAction(message){
+    switch(message["action"]){
+      
+      // The opponent resigned, so let's leave this screen
+      case 'resigned':
+        Navigator.of(context).pop();
+        break;
+
+      // The opponent played a move. So record it and rebuild the board
+      case 'play':
+        var data = (message["data"] as String).split(';');
+        
+        controller.makeMove(data[0],  data[1]);
+        // Force rebuild
+        setState((){});
+        break;
+    }
   }
 
   @override
