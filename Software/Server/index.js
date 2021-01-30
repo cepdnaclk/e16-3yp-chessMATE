83caaf1d46874fe84860f5af9fb0d81e0ca2fce1
DamsyDeSilva <damsydesilva@gmail.com>
2021-01-30 23:48:40 +0530

    define server port and create http servr and websocket server

---
+++ Software/Server/index.js
@@ -0,0 +1,24 @@
+// Adapt to the listening port number you want to use
+var webSocketsServerPort = 3000; 
+
+// websocket and http servers
+var webSocketServer = require('websocket').server;
+var http = require('http');
+
+/**
+* HTTP server to implement WebSockets
+*/
+var server = http.createServer(function(request, response) {});
+
+server.listen(webSocketsServerPort, function() {
+    console.log((new Date()) + " Server is listening on port "
+        + webSocketsServerPort);
+  });
+
+/**
+* WebSocket server
+*/
+var wsServer = new webSocketServer({
+    // WebSocket server is tied to a HTTP server. WebSocket request is just an enhanced HTTP request. 
+    httpServer: server
+});
