4206f3b9b5802d4193af72b851f879d195448741
DamsyDeSilva <damsydesilva@gmail.com>
2021-03-05 18:28:32 +0530

    Variable defined for LedPanel

--- Hardware/chessBoardCode/src/main.cpp
+++ Hardware/chessBoardCode/src/main.cpp
@@ -1,5 +1,27 @@
 #include <Arduino.h>
 
+// Define Pin Numbers for the Esp32 Dev Board
+// connections to shift Registers of LedPanel
+int dataPin = 2;
+int latchPin = 4;
+int clockPin = 5;
+
+// Arrays to hold values to be send to shift registers
+int anode_values[8];
+int red_values[8];
+int green_values[8];
+int blue_values[8];
+
+// Final array to send the shift registers with correct order
+int register_values[32];
+
+// Details of the movement to be displayed (should recieve via bluetooth)
+String moveNotation, moveEnd, moveStart;
+
+// Details should extract from the movement details
+int startRow, startCol;
+int endRow, endCol;
+
 void setup() {
   // put your setup code here, to run once:
 }
