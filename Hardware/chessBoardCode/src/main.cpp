6661cfd296af63db3e1f22a4a1c66a0024bfba64
DamsyDeSilva <damsydesilva@gmail.com>
2021-03-05 23:48:56 +0530

    modify displayPanel() to display path of a movement

--- Hardware/chessBoardCode/src/main.cpp
+++ Hardware/chessBoardCode/src/main.cpp
@@ -22,6 +22,9 @@ String moveNotation, moveEnd, moveStart;
 int startRow, startCol;
 int endRow, endCol;
 
+// variable for multiplexing
+int col_position = 0;
+
 // cells to be lightup in the matrix
 // 1 - start square (Red), 2 - end square (green), 3 - Path of the movement (blue) 
 int cells[8][8] = {
@@ -149,6 +152,22 @@ void displayPanel(){
 
   writeRegisters();
   clearRegisters();
+
+  // display path in blue
+  for (int row = 0; row < 8; row++){
+    if(cells[row][col_position] == 3){
+      // display end point of the movement in Blue
+      setRegisterPin('A', endRow, HIGH);
+      setRegisterPin('R', endCol, HIGH);
+      setRegisterPin('G', endCol, HIGH);
+      setRegisterPin('B', endCol, LOW);
+    }
+  }
+  writeRegisters();
+  col_position++;
+  if(col_position == 8){
+    col_position = 0;
+  }
 }
 
 // function to decode the move Notations and store in cells[][]
@@ -163,7 +182,6 @@ void decodeMove(String moveNotation, String moveEnd, String moveStart){
       startRow = moveStart[1] - '0'- 1;
       startCol = i;
       cells[startRow][startCol] = 1;
-      // break;
     }
 
     // set the end square in the cells 2D array
@@ -171,16 +189,6 @@ void decodeMove(String moveNotation, String moveEnd, String moveStart){
       endRow = moveEnd[1] - '0' - 1;
       endCol = i;
       cells[endRow][endCol] = 2;
-      // break;
     }
   }
-  // for (int i = 0; i < 8; i++){
-  //   // set the end square in the cells 2D array
-  //   if(moveEnd[0] == boardCols[i]){
-  //     endRow = moveEnd[1] - '0' - 1;
-  //     endCol = i;
-  //     cells[endRow][endCol] = 2;
-  //     break;
-  //   }
-  // }
 }
