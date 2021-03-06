8f595a65f3f93a7caa2e6df4e20b1b65fbf07ea6
DamsyDeSilva <damsydesilva@gmail.com>
2021-03-07 00:58:37 +0530

    basic bluetooth configurations added

--- Hardware/chessBoardCode/src/main.cpp
+++ Hardware/chessBoardCode/src/main.cpp
@@ -1,4 +1,16 @@
 #include <Arduino.h>
+#include "BluetoothSerial.h"    // for Serial Bluetooth
+
+// check if Bluetooth is properly enabled.
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+// Object for bluetooth
+BluetoothSerial ESP_BT; 
+
+// To store the recieving msg via bluetooth
+String bt_message;  
 
 // Define Pin Numbers for the Esp32 Dev Board
 // connections to shift Registers of LedPanel
@@ -47,6 +59,11 @@ void setRegisterPin(char regType, int index, boolean value);
 void decodeMove(String moveNotation, String moveEnd, String moveStart);
 
 void setup() {
+
+  Serial.begin(115200);
+  ESP_BT.begin("chessMATE"); // Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+  
   // LedPanel
   pinMode(dataPin, OUTPUT);
   pinMode(latchPin, OUTPUT);
@@ -64,6 +81,10 @@ void setup() {
 }
 
 void loop() {
+
+  if (ESP_BT.available()){
+
+  }
   displayPanel();
 }
 
@@ -347,7 +368,7 @@ void decodeMove(String moveNotation, String moveEnd, String moveStart){
       }
     }
   }
-  // King
+  // 6. King
   else if((moveNotation.length() == 3) && (String(moveNotation[0]).equals("K"))){
     // -> no path for kings normal movements
     // todo: Path exists only for castling
