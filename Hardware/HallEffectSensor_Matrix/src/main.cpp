7916c9da4adf6d7c81e957148d026dd7b72e34ef
isu92neth <123csissa@gmail.com>
2021-05-22 01:13:26 +0530

    Added definitions for chess pieces and spaces

--- Hardware/HallEffectSensor_Matrix/src/main.cpp
+++ Hardware/HallEffectSensor_Matrix/src/main.cpp
@@ -19,6 +19,22 @@ BluetoothSerial ESP_BT;
 #define RETRIEVE 0
 #define STORE 1
 
+// chess pieces and spaces definitions
+#define BORDER 255
+#define EMPTY 0
+#define BLACK_PAWN 1
+#define BLACK_KNIGHT 3
+#define BLACK_BISHOP 4
+#define BLACK_ROOK 5
+#define BLACK_QUEEN 9
+#define BLACK_KING 16
+#define WHITE_PAWN 129
+#define WHITE_KNIGHT 131
+#define WHITE_BISHOP 132
+#define WHITE_ROOK 133
+#define WHITE_QUEEN 137
+#define WHITE_KING 144
+
 void setup() {
   // put your setup code here, to run once:
 }
