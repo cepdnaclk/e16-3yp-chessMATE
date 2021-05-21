08ab712c9c797049032aab7110d6843c391dd776
isu92neth <123csissa@gmail.com>
2021-05-22 01:02:12 +0530

    Defined BluetoothSerial object

--- Hardware/HallEffectSensor_Matrix/src/main.cpp
+++ Hardware/HallEffectSensor_Matrix/src/main.cpp
@@ -1,7 +1,15 @@
 #include <Arduino.h>
 
 // for Serial Bluetooth
-#include "BluetoothSerial.h"  
+#include "BluetoothSerial.h" 
+
+// check if Bluetooth is properly enabled.
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+// Object for bluetooth
+BluetoothSerial ESP_BT;
 
 void setup() {
   // put your setup code here, to run once:
