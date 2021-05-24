d542d7e8849570621d87166aaa0dded772e0c222
isu92neth <123csissa@gmail.com>
2021-05-25 04:15:43 +0530

    Added Bluetooth object

--- Hardware/ChessBoardAlgorithm/src/main.cpp
+++ Hardware/ChessBoardAlgorithm/src/main.cpp
@@ -3,6 +3,14 @@
 // for Serial Bluetooth
 #include "BluetoothSerial.h" 
 
+// check if Bluetooth is properly enabled.
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+// Object for bluetooth
+BluetoothSerial ESP_BT;
+
 void setup() {
   // put your setup code here, to run once:
 }
