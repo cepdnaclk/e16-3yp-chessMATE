5fc7106a95bcd408feb70bb105a0e7afbdbe1ca9
DamsyDeSilva <damsydesilva@gmail.com>
2021-05-25 09:01:37 +0530

    new data added

--- Software/chessMATE_app/lib/backEnd_conn/websockets.dart
+++ Software/chessMATE_app/lib/backEnd_conn/websockets.dart
@@ -5,7 +5,7 @@ import 'package:web_socket_channel/io.dart';
 WebSocketsNotifications sockets = new WebSocketsNotifications();
 
 // substitute your server's IP and port
-const YOUR_SERVER_IP = '192.168.43.31';
+const YOUR_SERVER_IP = '192.168.8.100';
 const YOUR_SERVER_PORT = '3000';
 const String _SERVER_ADDRESS = 'ws://$YOUR_SERVER_IP:$YOUR_SERVER_PORT';
 
